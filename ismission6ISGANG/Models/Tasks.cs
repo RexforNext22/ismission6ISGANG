6f8700e9a7ad1ef11c3b9e80d0c005a1e855b0e7
Jacob Poor <jacob.poor98@gmail.com>
2022-02-08 11:38:01 -0700

    Jacob models

---
+++ ismission6ISGANG/Models/Tasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ismission6ISGANG.Models
+{
+    public class Tasks
+    {
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+        [Required]
+        public string Task { get; set; }
+        public string Date { get; set; }
+        [Required]
+        public string Quadrant { get; set; }
+        [Required]
+        public string Category { get; set; }
+        public bool Completed { get; set; }
+    }
+}
