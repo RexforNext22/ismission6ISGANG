7b4264048114266055d310169370fafb4895b23e
kevg14 <90358194+kevg14@users.noreply.github.com>
2022-02-08 21:08:10 -0700

    final

--- ismission6ISGANG/Models/Category.cs
+++ ismission6ISGANG/Models/Category.cs
@@ -5,6 +5,7 @@ namespace ismission6ISGANG.Models
 {
     public class Category
     {
+        //new Models Created In order to seperate it from Tasks and create a dropdown menu
         [Key]
         [Required]
         public int CategoryId { get; set; }
