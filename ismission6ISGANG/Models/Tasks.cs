c2e37d85950e0f1d319d434bd4c43fee1f897474
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2022-02-08 21:07:22 -0700

    added by Ryan

--- ismission6ISGANG/Models/Tasks.cs
+++ ismission6ISGANG/Models/Tasks.cs
@@ -8,23 +8,30 @@ namespace ismission6ISGANG.Models
 {
     public class Tasks
     {
+        // Task id field required and it is the primary key
         [Key]
         [Required]
         public int TaskID { get; set; }
 
+        // The field to input the task text
         [Required]
         public string Task { get; set; }
 
+        // Field to hold the date
         public string Date { get; set; }
 
+        // Field to hold which quadrant it is and also required
         [Required]
         public int Quadrant { get; set; }
 
+        // Set up the primary key foriegn key relationship withe category
         [Required]
         public int CategoryId { get; set; }
+
+        // Link to the category model
         public Category Category { get; set; }
 
-
+        // Field to hold the completed boolean value
         public bool Completed { get; set; }
     }
 }
